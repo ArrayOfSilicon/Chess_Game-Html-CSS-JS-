064fbea2bc8418dcd1ccca606ecafdad88c845c5
DharmikGohil <dharmikg86@gmail.com>
2022-06-26 13:15:58 +0530

    Initial Commit

---
+++ logic/click-handling.js
@@ -0,0 +1,26 @@
+const allSquares = document.getElementsByClassName("square");
+// console.log(allSquares);
+
+const clickedElements = [];
+
+for (const x of allSquares) {
+  x.addEventListener("click", function () {
+    // push element in empty Array
+    console.log(clickedElements);
+
+    const idOfElement = x.getAttribute("id");
+    const innerHtmlOfElement = document.getElementById(idOfElement).innerHTML;
+    if (clickedElements.length > 0) {
+      clickedElements[0].removeAttribute("style");
+      clickedElements.length = 0;
+    }
+    console.log(x);
+    if (
+      innerHtmlOfElement.includes("black") ||
+      innerHtmlOfElement.includes("white")
+    ) {
+      document.getElementById(idOfElement).style.backgroundColor = "yellow";
+      clickedElements.push(x);
+    }
+  });
+}
---
+++ logic/mapping-initial.js
@@ -0,0 +1,23 @@
+// selecting files using dom and storing into array
+
+const arrayOfFiles = document.querySelectorAll(".files");
+
+const filesNameArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+// counter for filesArray
+
+let fileNumber = 0;
+
+for (const i of arrayOfFiles) {
+  // counter variable of ranks(row)
+  let counter = 1;
+  for (const el of i.children) {
+    el.setAttribute("id", filesNameArray[fileNumber] + counter);
+    counter++;
+  }
+  fileNumber++;
+}
+
+for (const i of arrayOfFiles) {
+  // console.log(i.children);
+}
