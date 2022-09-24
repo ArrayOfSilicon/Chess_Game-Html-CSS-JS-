b8ecd4d4fcad16a2e710311cf9bce1ea3b2704f9
Dharmik Gohil <dharmikg86@gmail.com>
2022-09-24 10:36:00 +0530

    added ids to files, rank & each square

--- oop/renderHtml.js
+++ oop/renderHtml.js
@@ -5,16 +5,25 @@ const renderUI = function (squares) {
   // select mainContainer from my markup
   const mainContainer = document.querySelector(".main-container");
 
+  let rankNumber = 8;
+
   // create square of size 70 x 70
   squares.forEach((element) => {
     // rank is row and we have 8 ranks
     let rank = document.createElement("div");
     rank.classList.add("rank-style");
+    rank.setAttribute("id", rankNumber--);
     mainContainer.appendChild(rank);
     element.forEach((el) => {
       const square = document.createElement("div");
       square.classList.add(`color-${el.color}`);
       square.classList.add("square-common");
+
+      // assign unique id to each square
+      square.setAttribute("id", el.id);
+
+      // add file class to each square
+      square.classList.add("file-" + el.id[0]);
       rank.appendChild(square);
     });
   });
